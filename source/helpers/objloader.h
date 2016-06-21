2cac71144c5dcdef050fb1620f3c78401322783f
Robert Janca <robert@indoo.rs>
2016-06-21 15:22:43 +0200

    Restructure project, addsub modules

---
+++ source/helpers/objloader.h
@@ -0,0 +1,15 @@
+#ifndef OBJLOADER_H
+#define OBJLOADER_H
+
+#include "../objects/mesh.h"
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+#include <QStringList>
+
+class ObjLoader {
+public:
+  static Mesh *loadObj(QString fileName);
+};
+
+#endif // OBJLOADER_H
